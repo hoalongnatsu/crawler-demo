a4ad28417d16cbe8126d470b8bf89d1ea1ff2084
Hàn Lập <quan@Huynhs-Mac-mini.local>
2024-01-29 17:40:05 +0700

    init kafka connector for postgres

---
+++ configs/run.sh
@@ -0,0 +1,7 @@
+# Add connector
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @postgres-source.json
+
+# Check
+curl -s "http://localhost:8083/connectors?expand=info&expand=status" | \
+  jq '. | to_entries[] | [ .value.info.type, .key, .value.status.connector.state,.value.status.tasks[].state,.value.info.config."connector.class"]|join(":|:")' | \
+  column -s : -t| sed 's/\"//g'| sort
