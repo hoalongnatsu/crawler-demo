68ae05c9810a899a1d9e6ee1fd101b97e8fd3af1
Hàn Lập <quan@Huynhs-Mac-mini.local>
2024-01-30 10:39:05 +0700

    init kafka connector for elasticsearch

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 Crawler System: crawler posts + redis stream + consumer
 
 ![picture](./images/crawler.png)
+
+Change data capture system: Postgres + Kafka Connect + Elasticsearch
+
+![picture](./images/cdc.png)
--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 # Add connector
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @postgres-source.json
 
-# Check
+# Check connector added
+curl -s localhost:8083/connector-plugins | jq '.[].class'|egrep 'PostgresConnector|ElasticsearchSinkConnector'
+
+# Check connector running
 curl -s "http://localhost:8083/connectors?expand=info&expand=status" | \
   jq '. | to_entries[] | [ .value.info.type, .key, .value.status.connector.state,.value.status.tasks[].state,.value.info.config."connector.class"]|join(":|:")' | \
   column -s : -t| sed 's/\"//g'| sort
