98b36d90a66fa3a14c938c29e761b27e9e786266
hoalongnatsu <hoalongnatsu@gmail.com>
2024-03-06 18:09:33 +0700

    add grafana tempo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # Add connector
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/postgres-source.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/connector/postgres-source.json
 
 # Check connector added
 curl -s localhost:8083/connector-plugins | jq '.[].class'|egrep 'PostgresConnector|ElasticsearchSinkConnector'
