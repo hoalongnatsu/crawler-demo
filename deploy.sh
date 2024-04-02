9dfdd4446b9ca9c6a5a53a90ac6bff7b8a2e63aa
hoalongnatsu <hoalongnatsu@gmail.com>
2024-04-02 17:40:53 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 IMAGE=$1
 NEW_IMAGE=$2
 
-sed -i "s|$IMAGE.*|$NEW_IMAGE|g" docker-compose-server.yaml
+sudo sed -i "s|$IMAGE.*|$NEW_IMAGE|g" docker-compose-server.yaml
 docker compose -f docker-compose-server.yaml up -d
